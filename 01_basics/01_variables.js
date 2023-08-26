8d88a7580f85b96b28c426fef0d2d508ead0e778
Pratham Chouhan <109415931+chouhanpratham@users.noreply.github.com>
2023-08-26 10:31:37 +0000

    const_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "prathamchouhan12@gmail.com"
+var accountPassword = "123456";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2;
+accountEmail = "pc@pc.com";
+accountPassword = "212121";
+accountCity = "Pune";
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
+/*
+Prefer not to use var
+Because of issue in Blockscope & FunctionalScope
+*/

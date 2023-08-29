a2adbc251e3ba4d911d08635aa0446f8b1addaaa
Pratham Chouhan <109415931+chouhanpratham@users.noreply.github.com>
2023-08-29 12:34:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3+3); 
+console.log("Pratham"); //code readability should be high
+
+let name = "Pratham"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//Primitive Datatypes
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object => we will use extensively
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
